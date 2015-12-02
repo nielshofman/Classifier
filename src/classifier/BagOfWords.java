d93b0caee406e0009ba6f51743b4d637a662eac3
rvording <robbinvording@hotmail.com>
2015-12-02 14:47:04 +0100

    JE MOET JE BEK HOUDEN JIJ JOH

--- src/classifier/BagOfWords.java
+++ src/classifier/BagOfWords.java
@@ -4,17 +4,28 @@ import java.util.*;
 
 public class BagOfWords {
 	
-	private HashMap<Integer, String> listA;
-	private HashMap<Integer, String> listB;
+	public HashMap<BagOfWords, Integer> bags = new HashMap<BagOfWords, Integer>();
+	public List<String> words;
+	public int id;
+	public String name;
 	
-	public BagOfWords(){
-	
-		
-		
+	public BagOfWords(String name){
+		if (bags.isEmpty()) {
+			bags.put(this, 1);
+		}
+		for(int q = 1; q > 0; q++){
+			if(!bags.containsValue(q)){
+				bags.put(this, q);
+			}
+		}
+		this.name = name;
+		words = new LinkedList<String>();
 	}
 	
-	private void addWords(){
-		
+	private void addWords(List<String> in){
+		for(String word : in) {
+			words.add(word);
+		}
 	}
 
 }
