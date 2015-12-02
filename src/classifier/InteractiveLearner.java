0190c3fd496f790bede951f2fd1f41aceedc054b
rvording <robbinvording@hotmail.com>
2015-12-02 10:59:59 +0100

    First version of an easy tokenizer

--- src/classifier/InteractiveLearner.java
+++ src/classifier/InteractiveLearner.java
@@ -1,10 +1,18 @@
 package classifier;
+// http://www.java-samples.com/showtutorial.php?tutorialid=236
+import java.util.StringTokenizer;
+import java.io.*;
 
 public class InteractiveLearner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String out = "";
+		String in = "<author> = henkdegroot@hotmail.com Heee    &^&*^*)_+:}   FI=X dEZ*e && ShizZlE $#%^&";
+		StringTokenizer st = new StringTokenizer(in, "=;*/!@#$%^&*()-=_+[]{}|;,/:<>?");
+		while(st.hasMoreTokens()) {
+			String val = st.nextToken();
+			out = out + val;
+		} 
+		System.out.println(out.toLowerCase().replaceAll(" +", " "));
 	}
-
 }
