3fc9e2292a3d0956c53340f537ea8ed98df89cdb
nielshofman <niels.hofman@hotmail.com>
2015-12-02 14:21:41 +0100

    klasivier

--- src/classifier/Classifier.java
+++ src/classifier/Classifier.java
@@ -6,36 +6,35 @@ import java.util.*;
 public class Classifier {
 	
 	public List<String> tokenizedList;
-	private List<String> classifiedList;
 	private List<BagOfWords> bags;
-	private BagOfWords winningBag;
 	private HashMap<String, Integer> mapje = new HashMap<String, Integer>();
+	private HashMap<BagOfWords, Double> probabilities;
 
 	
 	public Classifier(List<String> words, List<BagOfWords> bags){
 		this.bags = new LinkedList<BagOfWords>();
-		this.classifiedList = new LinkedList<String>();
 		this.tokenizedList = words;
-		this.classifiedList = words;
-
 	}
 	
 	public void classify(List<String> unClassified){
 		
 		for (BagOfWords bag : bags){
-			classifyPerBag(unClassified, bag);
+			probabilities.put(bag ,classifyPerBag(unClassified, bag));
 		}
-		
 	}
 	
-	public void classifyPerBag(List<String> unClassified, BagOfWords bag){
+	public double classifyPerBag(List<String> unClassified, BagOfWords bag){
 		List<Integer> waardes = new LinkedList<Integer>();
+		double chanceOfBag = 1; 
 		for(String word : unClassified){
 			if (mapje.containsKey(word)) {
 				waardes.add((mapje.get(word)+1)/(mapje.size()+2));
-			}
-		
+			}	
 		}
+		for(int i = 0; i < waardes.size(); i++){
+				chanceOfBag = chanceOfBag * waardes.get(i);
+			}
+		return chanceOfBag;
 	}
 	
 	public static void main(String[] args) {
