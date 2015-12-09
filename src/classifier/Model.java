a93e36498200b411ccb2ab933f50086f5eb8773f
nielshofman <niels.hofman@hotmail.com>
2015-12-09 11:14:12 +0100

    ahit

--- src/classifier/Model.java
+++ src/classifier/Model.java
@@ -1,9 +1,16 @@
 package classifier;
 
+import java.io.*;
 import java.util.*;
+
+import sun.misc.IOUtils;
 
 public class Model {
 
+	public static final String menTestLink = "C:/Users/Niels/git/Classifier/src/train/Mannentrain/M-test11.txt";
+	public static final String menTestName = "Mentest";
+	public static final String womenTestLink = "https://github.com/nielshofman/Classifier/tree/master/src/train/Vrouwentrain";
+	public static final String womenTestName = "WomenTest";
 	public List<BagOfWords> bags;
 	public InteractiveLearner il;
 	
@@ -12,8 +19,26 @@ public class Model {
 		
 	}
 	
-	public static void main(String[] args) {
-		Scanner user_input = new Scanner(System.in);
+	private String getFile(String fileName){
+		
+		  String result = "";
+			
+		  ClassLoader classLoader = getClass().getClassLoader();
+		  try {
+			result = IOUtils.toString(classLoader.getResourceAsStream(fileName));
+		  } catch (IOException e) {
+			e.printStackTrace();
+		  }
+			
+		  return result;
+			
+	  }
+	
+	
+	public static void main(String[] args) throws IOException {
+		Model ml = new Model(3);
+		
+		/*Scanner user_input = new Scanner(System.in);
 		Model model = new Model(2);
 		InteractiveLearner il = new InteractiveLearner();
 		String input;
@@ -26,6 +51,19 @@ public class Model {
 			}
 		}
 		user_input.close();
-		il.getWords();
+		il.getWords(); 
+	}
+	//http://www.homeandlearn.co.uk/java/read_a_textfile_in_java.html
+	public List<String> getWords() throws IOException{
+		FileReader fr = new FileReader(menTestLink);
+		BufferedReader textReader = new BufferedReader(fr);
+		
+		List<String> textFile = new LinkedList<String>();
+		
+		for (int i=0; i <= 1000000; i++){
+			textFile.add(textReader.readLine());
+		}
+		textReader.close();
+		return textFile;*/
 	}
 }
